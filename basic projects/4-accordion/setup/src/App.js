454083d08848929547ac496b417172f797187f1c
Leonardo <leonoman07@gmail.com>
2021-04-11 10:34:38 -0300

    Accordion

--- basic projects/4-accordion/setup/src/App.js
+++ basic projects/4-accordion/setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <div>
+      <main>
+        <div className="container">
+          <h3>quetions and answers about login</h3>
+          <section className="info">
+            {questions.map((question) => {
+              return (
+                <SingleQuestion
+                  key={question.id}
+                  {...question}
+                ></SingleQuestion>
+              );
+            })}
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
