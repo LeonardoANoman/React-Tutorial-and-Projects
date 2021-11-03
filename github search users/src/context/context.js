e443f62bce8dc797b5612f5091c77e1ba4c0a954
Leonardo <leonoman07@gmail.com>
2021-11-03 09:37:47 -0300

    context, search and dashboard

--- github search users/src/context/context.js
+++ github search users/src/context/context.js
@@ -13,19 +13,28 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
   const [requests, setRequests] = useState(0);
-  const [loading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState({ show: false, msg: "" });
 
   const searchGithubUser = async (user) => {
     toggleError();
+    setIsLoading(true);
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
       console.log(err)
     );
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      axios(`${rootUrl}/users/${login}/repos?per
+      _page=100`).then((response) => setRepos(response.data));
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      );
     } else {
       toggleError(true, "there is no user with that username");
     }
+    checkRequests();
+    setIsLoading(false);
   };
 
   const checkRequests = () => {
@@ -57,6 +66,7 @@ const GithubProvider = ({ children }) => {
         requests,
         error,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
